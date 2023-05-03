486e86dc6228233f3b65d9692d2f7f941301cfcf
johanbuntinx <johan.buntinx@gmail.com>
2023-05-03 08:31:38 +0200

    IMJV-6617: Netkernel: kleuren aanpassen

--- dev/webpack.config.base.js
+++ dev/webpack.config.base.js
@@ -23,8 +23,10 @@ module.exports = {
         new CopyWebpackPlugin([
             { from: './src/img/favicon.ico', to: 'img/favicon.ico' },
             { from: './src/img/marker-selected.png', to: 'img/marker-selected.png' },
+            { from: './src/img/marker.png', to: 'img/marker.png' },
             { from: './src/img/hero.png', to: 'img/hero.png' },
             { from: './src/img/hero-light.png', to: 'img/hero-light.png' },
+            { from: './src/img/logo-domg.png', to: 'img/logo-domg.png' },
             { from: './src/queries', to: '../queries' },
             { from: './src/scripts', to: '../scripts' }
         ])
